b9e87ecdc93c083ba313ce9e87fe56c7ffb41e47
Dany Cooper <danycooper@MacBook-Pro-de-Dany.local>
2017-08-25 19:05:21 -0400

    Update 2

--- js/political.js
+++ js/political.js
@@ -1,12 +1,31 @@
 //POLITICAL
-var political = "Daniel Cooper Bermudez is a political scientist, aspiring programmer, and translator currently based in Maracaibo, Venezuela. Daniel has also lived and worked in Philadelphia, PA and Immokalee, FL in the past few years. He is an active community member wherever he resides, always seeking to build relationships through shared callings and political organizing. He has worked with the Coalition of Immokalee Workers as a digital archivist and organizer, with Philadelphia Councilwoman María Quiñones Sánchez as an intern, and with Popular Resistance as a writer and on web content."
+var political = `
+<a target="_blank" href="http://popularresistance.org">Popular Resistance</a><br>
+Writer and fundraiser<br>
+Remote, Sept 2016 - Present<br><br>
+
+<a target="_blank" href="http://codhez.org.ve/">Comisión de Derechos Humanos del Estado Zulia</a><br>
+Organizer and website manager<br>
+Maracaibo, Venezuela<br>
+April 2017 - Present<br><br>
+
+<a target="_blank" href="http://ciw-online.org/">Coalition of Immokalee Workers</a><br>
+Organizer<br>
+Immokalee, FL<br>
+Aug 2015 – Aug 2016<br><br>
+
+<a target="_blank" href="http://phlcouncil.com/MariaQSanchez">Office of Councilwoman Quiñones Sánchez</a><br>
+Intern<br>
+Philadelpa, PA<br>
+Sept. Jan – June 2015<br><br>
+`
 
 $("#political").on("click", function() {
 	$("#textbox").css({"opacity":"0"});
 	setTimeout(function(){
 		$("h1").text("Political Work");
 		$("h3").text("Political Work");
-		$("#texto").text(political);
+		$("#texto").html(political);
 	}, 1000);
 	setTimeout(function(){
 		$("#textbox").css({"opacity":"1"});
@@ -45,7 +64,13 @@ $("#webdev").on("click", function() {
 });
 
 //WEB DEV
-var translate = "Daniel Cooper Bermudez is a political scientist, aspiring programmer, and translator currently based in Maracaibo, Venezuela. Daniel has also lived and worked in Philadelphia, PA and Immokalee, FL in the past few years. He is an active community member wherever he resides, always seeking to build relationships through shared callings and political organizing. He has worked with the Coalition of Immokalee Workers as a digital archivist and organizer, with Philadelphia Councilwoman María Quiñones Sánchez as an intern, and with Popular Resistance as a writer and on web content."
+var translate = `
+Daniel has ample experience translating and interpreting from English to Spanish and Spanish to English in many formats.
+He has translated documents to be sent to the United Nations for indigenous peoples' and Venezuelan organizations,
+articles for the grassroots worker organizations in the United States, political analysis pieces from community voices in 
+Venezuela, press releases, flyers, informational pamphlets, and even a feature length indigenous film from Brazil
+'Time was Endless'.
+`
 
 $("#translate").on("click", function() {
 	$("#textbox").css({"opacity":"0"});
